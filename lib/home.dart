4ab491eced16557912d15d6c22b7f58c02185763
kashyapsandesh <sandeshghimire202@gmail.com>
2023-01-25 12:38:13 +0545

    added value

---
+++ lib/home.dart
@@ -0,0 +1,42 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  final controller = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: TextFormField(
+          controller: controller,
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {
+                final name = controller.text;
+                createUser(nameT: name);
+              },
+              icon: Icon(Icons.add))
+        ],
+      ),
+    );
+  }
+
+  Future createUser({required String nameT}) async {
+    final docUser =
+        FirebaseFirestore.instance.collection('user').doc('mero-id');
+    final json = {
+      'name': nameT,
+      'age': 21,
+    };
+    //create document and write data to firebase
+    await docUser.set(json);
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import "package:flutter/material.dart";
+
+import 'home.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          primaryColor: Colors.amber,
+          appBarTheme: AppBarTheme(backgroundColor: Colors.pink)),
+      home: Homepage(),
+    );
+  }
+
+ 
+}
